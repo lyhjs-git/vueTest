6d1866cc5796d73702167ee0b16aa4d6c420a5af
李云辉 <874085121@qq.com>
2020-12-13 17:09:22 +0900

    数据库修改

--- route/index.js
+++ route/index.js
@@ -4,7 +4,7 @@ const common = require('../libs/common');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: '123456',
     database: 'tenji'
 });
 module.exports = () => {
